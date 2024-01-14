ee0affa3f8dffe64a190efa5e10687878dafa03f
Genevieve Leanne Dominguez <93393395+genevieveleanne@users.noreply.github.com>
2024-01-14 14:05:38 -0500

    Added word on page load. Edited pronunciation code to display first audio link if the second one is null.

--- app.js
+++ app.js
@@ -7,14 +7,21 @@ function retrieveUserInput(event) {
 
   let userWord = document.querySelector("#user-word");
 
-  let wordDefinition = `${apiBase}/${userWord.value}`;
-  axios.get(wordDefinition).then(displayUserInput);
+  searchWord(userWord.value);
 }
 
 let form = document.querySelector("form");
 form.addEventListener("submit", retrieveUserInput);
 
-//Display word definition, pronunciation, etc.
+//Display word on page load
+function searchWord(word) {
+  let wordDefinition = `${apiBase}/${word}`;
+  axios.get(wordDefinition).then(displayUserInput);
+}
+
+searchWord("sunrise");
+
+//Display word definition and pronunciation
 function displayUserInput(response) {
   console.log(response.data);
 
@@ -22,6 +29,18 @@ function displayUserInput(response) {
   word.innerHTML = response.data[0].word;
 
   let pronunciationLink = `${response.data[0].phonetics[1].audio}`;
+
+  if (pronunciationLink === "") {
+    pronunciationDisplay(response);
+  } else {
+    let pronunciation = document.querySelector("#pronunciation");
+    pronunciation.innerHTML = `<a href="${pronunciationLink}" target="_blank">Pronunciation</a>`;
+  }
+}
+
+//If phonetics[1] is null, then inject link from phonetics[0]
+function pronunciationDisplay(response) {
+  let pronunciationLink = `${response.data[0].phonetics[0].audio}`;
 
   let pronunciation = document.querySelector("#pronunciation");
   pronunciation.innerHTML = `<a href="${pronunciationLink}" target="_blank">Pronunciation</a>`;
