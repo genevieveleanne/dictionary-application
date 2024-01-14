64485c8d96e18c8020daa084f1fde5d734b2f49e
Genevieve Leanne Dominguez <93393395+genevieveleanne@users.noreply.github.com>
2024-01-14 12:50:26 -0500

    Added API basepoint. Created functions to retrieve user input and display the word on the page.

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-alert("Hello World!");
+//API basepoint
+let apiBase = "https://api.dictionaryapi.dev/api/v2/entries/en";
+
+//User submits form
+function retrieveUserInput(event) {
+  event.preventDefault();
+
+  let userWord = document.querySelector("#user-word");
+
+  let wordDefinition = `${apiBase}/${userWord.value}`;
+  axios.get(wordDefinition).then(displayUserInput);
+}
+
+let form = document.querySelector("form");
+form.addEventListener("submit", retrieveUserInput);
+
+//Display Word Definition
+function displayUserInput(response) {
+  console.log(response.data);
+
+  let h2 = document.querySelector("h2");
+  h2.innerHTML = response.data[0].word;
+}
